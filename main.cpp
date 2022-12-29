8037909f4200557f6f5f10609bbc366905e91810
alibek <johnbuck_kebarty@mail.ru>
2022-12-29 22:24:57 +0300

    On branch master  Initial commit  Changes to be committed: 	new file:   CMakeLists.txt 	new file:   main.cpp  Untracked files: 	..gitignore.un~ 	.gitignore

---
+++ main.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#define lvl 10
+using namespace std;
+
+
+#include<stdio.h>
+
+int main()
+{
+    int A[lvl][lvl] = {
+            {0,0,0,1,0,0,0,0},
+            {0,0,1,0,0,0,0,0},
+            {0,0,0,0,1,0,0,0},
+            {0,1,1,0,1,0,1,0},
+            {0,0,0,0,0,1,0,0},
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,1,0,1},
+            {0,0,0,0,0,1,0,0}
+    },
+            M[lvl][lvl];
+    for (int i = 0; i < lvl; i++) {
+        for (int j = 0; j < lvl; j++) {
+            M[i][j] = A[i][j];
+        }
+    }
+    for (int k = 0; k < lvl; ++k) {
+        for (int i = 0; i < lvl; ++i) {
+            for (int j = 0; j < lvl; ++j) {
+                M[i][j] = M[i][j] | (M[i][k] & M[k][j]);
+            }
+        }
+    }
+
+    for (int i = 0; i < lvl; i++) {
+        for (int j = 0; j < lvl; j++) {
+            cout << " " << M[i][j];
+        }
+        cout << '\n';
+    }
+}
